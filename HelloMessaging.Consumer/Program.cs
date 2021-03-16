9128674030b08d3e4c5d93f7e2f16cd75c9db279
Murilo Beltrame <murilobeltrame@gmail.com>
2021-03-16 00:31:33 -0300

    1st commit

---
+++ HelloMessaging.Consumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloMessaging.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
