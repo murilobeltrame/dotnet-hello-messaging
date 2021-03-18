6a1b5790c46879f7f6d8a27fde9568de4d8c0435
Murilo Beltrame <murilobeltrame@gmail.com>
2021-03-18 00:20:49 -0300

    cleanning up

--- HelloMessaging.Lib/IBusMessage_T_.cs
+++
@@ -1,8 +0,0 @@
-namespace HelloMessaging.Lib
-{
-    public interface IBusMessage<T>
-    {
-        T body {get; set;}
-        string lockToken {get; set;}
-    }
-}
--- HelloMessaging.Lib/IBusService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace HelloMessaging.Lib
-{
-    public interface IBusService
-    {
-        IBusClient Client(string queuename);
-    }
-}
