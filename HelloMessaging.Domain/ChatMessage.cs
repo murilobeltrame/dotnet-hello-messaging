34732747c255117b775c515d20f44d58c9c11620
Murilo Beltrame <murilobeltrame@gmail.com>
2021-03-22 11:02:07 -0300

    added versioning for message contract

--- HelloMessaging.Domain/ChatMessage.cs
+++ HelloMessaging.Domain/ChatMessage.cs
@@ -3,5 +3,8 @@
     public interface IChatMessage
     {
         string Text { get; set; }
+#pragma warning disable IDE1006 // Naming Style for message Header expected by MassTransit
+        string __Version { get; set; }
+#pragma warning restore IDE1006 // Naming Style for message Header expected by MassTransit
     }
 }
